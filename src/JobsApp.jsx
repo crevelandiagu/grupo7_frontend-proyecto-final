9291c93e7ba2251ab194706436919c65c28f0528
arthur-ackernman <ca.castrov12@uniandes.edu.co>
2023-11-04 16:36:42 -0500

    hecho listar entrevista

--- src/JobsApp.jsx
+++ src/JobsApp.jsx
@@ -1,12 +1,14 @@
 
 // import { AuthProvider } from "./auth/context/AuthProvider"
+import { Interview } from "./candidate/views/Interview"
 import { AppRouter } from "./router/AppRouter"
 
 export const JobsApp = () => {
 
   return (
     // <AuthProvider>
-      <AppRouter />
+      // 
+      <Interview />
     // </AuthProvider>
   )
 }
