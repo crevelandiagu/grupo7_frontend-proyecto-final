5656e31902064f95d8877e86e51cd93773937771
arthur-ackernman <ca.castrov12@uniandes.edu.co>
2023-11-25 00:35:24 -0500

    hecho add test

---
+++ test/fixtures/companyStates.js
@@ -0,0 +1,13 @@
+export const initialState = {
+  status: null,
+  idCandidate: null,
+  view: 'dashboard',
+  errorMessage: null,
+};
+
+export const setView = {
+  status: null,
+  view: 'interview',
+  idCandidate: null,
+  errorMessage: undefined,
+};
